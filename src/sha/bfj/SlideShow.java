85ce611ab5fa7d3626a2a59afaa201d55d5f42b9
Raphael Jenni <jenni.raphael@gmail.com>
2019-02-28 20:13:14 +0100

    Fix addSlides to do it as the name says

--- src/sha/bfj/SlideShow.java
+++ src/sha/bfj/SlideShow.java
@@ -1,13 +1,14 @@
 package sha.bfj;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SlideShow {
   List<Slide> slides = new ArrayList<>();
 
-  public void addSlides(Slide slide) {
-    slides.add(slide);
+  public void addSlides(Slide... slide) {
+    slides.addAll(Arrays.asList(slide));
   }
 
 
